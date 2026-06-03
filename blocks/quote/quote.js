782ea033e16625864b9bf8799da7287e2ff9da11
gek262 <giancarlo262@gmail.com>
2026-06-03 22:15:43 +0200

    fix js quote

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,27 +1,7 @@
 export default function decorate(block) {
-  const [quoteWrapper, linkWrapper, titleLinkWrapper] = block.children;
+  const [quoteWrapper] = block.children;
 
-  if (quoteWrapper) {
-    const blockquote = document.createElement('blockquote');
-    blockquote.textContent = quoteWrapper.textContent.trim();
-    quoteWrapper.replaceChildren(blockquote);
-  }
-
-  if (linkWrapper && titleLinkWrapper) {
-    const linkUrl = linkWrapper.querySelector('a')?.href
-      || linkWrapper.textContent.trim();
-    const linkTitle = titleLinkWrapper.textContent.trim();
-
-    if (linkUrl && linkTitle) {
-      const finalLink = document.createElement('a');
-      finalLink.href = linkUrl;
-      finalLink.textContent = linkTitle;
-      finalLink.className = 'quote-link';
-
-      linkWrapper.replaceChildren(finalLink);
-      linkWrapper.classList.add('quote-link-container');
-
-      titleLinkWrapper.remove();
-    }
-  }
+  const blockquote = document.createElement('blockquote');
+  blockquote.textContent = quoteWrapper.textContent.trim();
+  quoteWrapper.replaceChildren(blockquote);
 }
