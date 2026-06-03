0324b7a1afbcfd696dbf6c41ce83f1743fed2c4d
gek262 <giancarlo262@gmail.com>
2026-06-03 22:24:16 +0200

    js link quote

--- blocks/quote/quote.js
+++ blocks/quote/quote.js
@@ -1,7 +1,18 @@
 export default function decorate(block) {
-  const [quoteWrapper] = block.children;
+  const [quoteWrapper, linkWrapper, titleLinkWrapper] = block.children;
 
   const blockquote = document.createElement('blockquote');
   blockquote.textContent = quoteWrapper.textContent.trim();
   quoteWrapper.replaceChildren(blockquote);
+
+  const linkUrl = linkWrapper.querySelector('a')?.href || linkWrapper.textContent.trim();
+  const linkTitle = titleLinkWrapper.textContent.trim();
+  if (linkUrl && linkTitle) {
+    const finalLink = document.createElement('a');
+    finalLink.href = linkUrl;
+    finalLink.textContent = linkTitle;
+    finalLink.className = 'quote-link';
+    linkWrapper.replaceChildren(finalLink);
+    linkWrapper.classList.add('quote-link-container');
+  }
 }
